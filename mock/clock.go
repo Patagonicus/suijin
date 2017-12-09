Patagonicus/log

Add mock for Clock

mock/clock.go
@@ -1,0 +1,44 @@
+package mock
+
+import (
+	"sync"
+	"time"
+
+	"github.com/Patagonicus/log"
+)
+
+// Clock is an implementation of log.Clock used for mocking.
+// It starts with a given time and increases that time by a fixed amount every time Now() is called.
+// Clock is safe to be used concurrently.
+type Clock struct {
+	current time.Time
+	step    time.Duration
+	lock    sync.Locker
+}
+
+// Clock has to implement log.Clock
+var _ log.Clock = &Clock{}
+
+// NewClock returns a new Clock. This Clock will return start the first time Now() is called.
+// Afterwards it will return start+step, start+2*step, …
+func NewClock(start time.Time, step time.Duration) *Clock {
+	return &Clock{start, step, new(sync.Mutex)}
+}
+
+func (c *Clock) Now() time.Time {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	now := c.current
+	c.current = c.current.Add(c.step)
+
+	return now
+}
+
+// Next returns the time the next call to Now will return. The difference to Now is that it will not increase the time.
+func (c *Clock) Next() time.Time {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	return c.current
+}

mock/clock_test.go
@@ -1,0 +1,88 @@
+package mock_test
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/Patagonicus/log/mock"
+)
+
+const goroutineCount = 20
+const iterationCount = 1000
+
+func ExampleClock() {
+	c := mock.NewClock(mustParseTime("5:06PM"), time.Minute)
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	// Output:
+	// 5:06PM
+	// 5:07PM
+	// 5:08PM
+	// 5:09PM
+}
+
+func ExampleClock_Next() {
+	c := mock.NewClock(mustParseTime("5:06PM"), time.Minute)
+	fmt.Println(c.Next().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Next().Format(time.Kitchen))
+	fmt.Println(c.Next().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	// Output:
+	// 5:06PM
+	// 5:06PM
+	// 5:07PM
+	// 5:08PM
+	// 5:08PM
+	// 5:08PM
+}
+
+func TestConcurrent(t *testing.T) {
+	start := mustParseTime("0:00AM")
+	c := mock.NewClock(start, time.Minute)
+	ch := make(chan []time.Time)
+
+	for i := 0; i < goroutineCount; i++ {
+		go func() {
+			result := make([]time.Time, 0, iterationCount)
+			for i := 0; i < iterationCount; i++ {
+				result = append(result, c.Now())
+			}
+			ch <- result
+		}()
+	}
+
+	result := make([]time.Time, 0, goroutineCount*iterationCount)
+	for i := 0; i < goroutineCount; i++ {
+		result = append(result, (<-ch)...)
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Before(result[j])
+	})
+
+	if result[0] != start {
+		t.Errorf("wrong start time, expected %s but got %s", start, result[0])
+	}
+	for i := 1; i < len(result); i++ {
+		diff := result[i].Sub(result[i-1])
+		if diff != time.Minute {
+			t.Errorf("difference between %d and %d is %s, not a minute", i-1, i, diff)
+			// if there's one problem here, there are probably a lot more. Stop the test so we don't spam the output.
+			t.FailNow()
+		}
+	}
+}
+
+func mustParseTime(t string) time.Time {
+	result, err := time.Parse(time.Kitchen, t)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}

Patagonicus/suijin

mock/clock.go
@@ -1,0 +1,44 @@
+package mock
+
+import (
+	"sync"
+	"time"
+
+	"github.com/Patagonicus/suijin"
+)
+
+// Clock is an implementation of suijin.Clock used for mocking.
+// It starts with a given time and increases that time by a fixed amount every time Now() is called.
+// Clock is safe to be used concurrently.
+type Clock struct {
+	current time.Time
+	step    time.Duration
+	lock    sync.Locker
+}
+
+// Clock has to implement suijin.Clock
+var _ suijin.Clock = &Clock{}
+
+// NewClock returns a new Clock. This Clock will return start the first time Now() is called.
+// Afterwards it will return start+step, start+2*step, …
+func NewClock(start time.Time, step time.Duration) *Clock {
+	return &Clock{start, step, new(sync.Mutex)}
+}
+
+func (c *Clock) Now() time.Time {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	now := c.current
+	c.current = c.current.Add(c.step)
+
+	return now
+}
+
+// Next returns the time the next call to Now will return. The difference to Now is that it will not increase the time.
+func (c *Clock) Next() time.Time {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	return c.current
+}

mock/clock_test.go
@@ -1,0 +1,88 @@
+package mock_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/Patagonicus/suijin/legacy"
+	"github.com/Patagonicus/suijin/mock"
+)
+
+const goroutineCount = 20
+const iterationCount = 1000
+
+func ExampleClock() {
+	c := mock.NewClock(mustParseTime("5:06PM"), time.Minute)
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	// Output:
+	// 5:06PM
+	// 5:07PM
+	// 5:08PM
+	// 5:09PM
+}
+
+func ExampleClock_Next() {
+	c := mock.NewClock(mustParseTime("5:06PM"), time.Minute)
+	fmt.Println(c.Next().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	fmt.Println(c.Next().Format(time.Kitchen))
+	fmt.Println(c.Next().Format(time.Kitchen))
+	fmt.Println(c.Now().Format(time.Kitchen))
+	// Output:
+	// 5:06PM
+	// 5:06PM
+	// 5:07PM
+	// 5:08PM
+	// 5:08PM
+	// 5:08PM
+}
+
+func TestConcurrent(t *testing.T) {
+	start := mustParseTime("0:00AM")
+	c := mock.NewClock(start, time.Minute)
+	ch := make(chan []time.Time)
+
+	for i := 0; i < goroutineCount; i++ {
+		go func() {
+			result := make([]time.Time, 0, iterationCount)
+			for i := 0; i < iterationCount; i++ {
+				result = append(result, c.Now())
+			}
+			ch <- result
+		}()
+	}
+
+	result := make([]time.Time, 0, goroutineCount*iterationCount)
+	for i := 0; i < goroutineCount; i++ {
+		result = append(result, (<-ch)...)
+	}
+
+	legacy.Slice(result, func(i, j int) bool {
+		return result[i].Before(result[j])
+	})
+
+	if result[0] != start {
+		t.Errorf("wrong start time, expected %s but got %s", start, result[0])
+	}
+	for i := 1; i < len(result); i++ {
+		diff := result[i].Sub(result[i-1])
+		if diff != time.Minute {
+			t.Errorf("difference between %d and %d is %s, not a minute", i-1, i, diff)
+			// if there's one problem here, there are probably a lot more. Stop the test so we don't spam the output.
+			t.FailNow()
+		}
+	}
+}
+
+func mustParseTime(t string) time.Time {
+	result, err := time.Parse(time.Kitchen, t)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}