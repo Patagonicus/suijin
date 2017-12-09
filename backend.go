03681f3c9b95bbd52a9dcc0e647829cb021bdb6a
Philipp Adolf <derphilippadolf@googlemail.com>
2017-12-09 18:20:08 +0100

    Add Backend

---
+++ backend.go
@@ -0,0 +1,47 @@
+package suijin
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
+// Backend is something that can store messages.
+type Backend interface {
+	// Writes a message to the backend. This must be safe to use concurrently.
+	Log(m Message)
+}
+
+// DefaultBackend writes messages to stderr.
+var DefaultBackend Backend = NewWriterBackend(os.Stderr, DefaultFormatter)
+
+type writerBackend struct {
+	formatter Formatter
+	writer    io.Writer
+	lock      *sync.Mutex
+}
+
+// NewWriterBackend returns a Backend that writes messages to an io.Writer.
+func NewWriterBackend(w io.Writer, f Formatter) Backend {
+	return writerBackend{
+		f,
+		w,
+		new(sync.Mutex),
+	}
+}
+
+func (w writerBackend) Log(m Message) {
+	b, err := w.formatter.Format(m)
+	if err != nil {
+		buf := new(bytes.Buffer)
+		fmt.Fprintf(buf, "error formatting log message: %s", err)
+		b = buf.Bytes()
+	}
+
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	w.writer.Write(b)
+	w.writer.Write([]byte{'\n'})
+}
---
+++ backend_test.go
@@ -0,0 +1,88 @@
+package suijin_test
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/Patagonicus/suijin"
+	"github.com/Patagonicus/suijin/legacy"
+)
+
+const goroutineCount = 20
+const iterationCount = 1000
+
+type mockFormatter struct{}
+
+func (f mockFormatter) Format(m suijin.Message) ([]byte, error) {
+	return []byte(m.Msg), nil
+}
+
+func TestBackend(t *testing.T) {
+	buf := new(bytes.Buffer)
+	b := suijin.NewWriterBackend(buf, mockFormatter{})
+	wg := new(sync.WaitGroup)
+	wg.Add(goroutineCount)
+
+	for i := 0; i < goroutineCount; i++ {
+		go func(index int) {
+			for i := 0; i < iterationCount; i++ {
+				b.Log(suijin.Message{
+					Msg: fmt.Sprintf("%d %d", index, i),
+				})
+			}
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	if buf.Bytes()[buf.Len()-1] != '\n' {
+		t.Fatalf("buffer does not end with a newline")
+	}
+
+	buf.Truncate(buf.Len() - 1)
+
+	type line struct {
+		routine, index int
+	}
+	var lines []line
+	for _, l := range strings.Split(buf.String(), "\n") {
+		s := strings.Split(l, " ")
+		if len(s) != 2 {
+			t.Fatalf("invalid line: %s", l)
+		}
+
+		r, err := strconv.Atoi(s[0])
+		if err != nil {
+			t.Fatalf("invalid line: %s", l)
+		}
+
+		i, err := strconv.Atoi(s[1])
+		if err != nil {
+			t.Fatalf("invalid line: %s", l)
+		}
+
+		lines = append(lines, line{r, i})
+	}
+
+	legacy.Slice(lines, func(a, b int) bool {
+		if lines[a].routine == lines[b].routine {
+			return lines[a].index < lines[b].index
+		}
+		return lines[a].routine < lines[b].routine
+	})
+
+	for i := 0; i < goroutineCount; i++ {
+		for j := 0; j < iterationCount; j++ {
+			index := i*iterationCount + j
+			expected := line{i, j}
+			if lines[index] != expected {
+				t.Fatalf("line %d is wrong, expected '%s' but got '%s'", index, expected, lines[index])
+			}
+		}
+	}
+}
