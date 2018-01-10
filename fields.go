0a9e2a5a72924760b43e0c678541c6f4e70466f4
Philipp Adolf <derphilippadolf@googlemail.com>
2018-01-10 15:48:35 +0100

    Add Fields.AddAll

--- fields.go
+++ fields.go
@@ -2,3 +2,10 @@ package suijin
 
 // Fields stores additional information for a log message.
 type Fields map[string]interface{}
+
+// AddAll adds everything from other to this Fields object. Existing entries are overwritten if they use the same key.
+func (f Fields) AddAll(other Fields) {
+	for k, v := range other {
+		f[k] = v
+	}
+}
